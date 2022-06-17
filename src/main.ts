be89e0e976c9434fd7a38260ec69c428bbdc5166
robit <36677806+robit-man@users.noreply.github.com>
2022-06-17 18:07:35 +0000

    tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { TroisJSVuePlugin } from 'troisjs'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { TroisJSVuePlugin } from 'troisjs';
 
-import App from './App.vue'
-import router from './router'
+import App from '/src/App.vue';
+import router from './router/';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(TroisJSVuePlugin)
+app.use(createPinia());
+app.use(router);
+app.use(TroisJSVuePlugin);
 
-app.mount('#app')
+app.mount('#app');
