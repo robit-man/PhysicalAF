c17ed1f5a117700f98d5230c4b6ef24d399ad341
robit <36677806+robit-man@users.noreply.github.com>
2022-06-17 20:20:11 +0000

    tsconfig

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia';
 import { TroisJSVuePlugin } from 'troisjs';
 
 import App from './App.vue';
-import router from './router';
+import router from './router/index.js';
 
 const app = createApp(App);
 
